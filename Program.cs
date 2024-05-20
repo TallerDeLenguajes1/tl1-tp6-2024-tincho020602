2085b003b395164834e106b321f30113255367c6
tincho020602 <ezequiel.ibarra020602@gmail.com>
2024-05-20 19:30:48 -0300

    ejercicio 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
+
+int invertido = 0, resto;
+Console.WriteLine("Ingrese un número: ");
+string cadena = Console.ReadLine();
+int numero;
+if (int.TryParse(cadena, out numero))
+{
+    if (numero > 0)
+    {
+        while (numero != 0)
+        {
+            resto = numero % 10;
+            invertido = invertido * 10 + resto;
+            numero /= 10;
+        }
+        Console.WriteLine("El número invertido es:" + invertido);
+    }
+    else
+    {
+        Console.WriteLine("El número debe ser mayor a 0.");
+    }
+}
+
+
+
+
+
+
+
+/* PUNTO QUE VENIA EN EL TP:
 Console.WriteLine("Hello, World!");
 
 int a;
@@ -7,3 +39,4 @@ a=10;
 b=a;
 Console.WriteLine("valor de a:"+a);
 Console.WriteLine("valor de b:"+b);
+    FIN*/
